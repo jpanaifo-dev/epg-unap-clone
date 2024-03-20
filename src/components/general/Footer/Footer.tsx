bc0fe377256c9956612678bcbdc75682cde2f0cf
fcancino-dev <franco.cancino@unapiquitos.edu.pe>
2024-03-20 10:03:08 -0500

    footer modify

--- src/components/general/Footer/Footer.tsx
+++ src/components/general/Footer/Footer.tsx
@@ -1,19 +1,46 @@
 'use client'
 import DataFooter from '@/utils/dataFooter.json'
 import Link from 'next/link'
+import { IconPhone, IconMail, IconLocation, IconBrandFacebook, IconBrandGithub, IconBrandInstagram } from '@tabler/icons-react'
+import { Divider, Image } from '@nextui-org/react'
+import epg from '@/assets/images/epg-logo.png'
+
+const icons = [
+  { icon: <IconLocation size={20} />, key: 'location' },
+  { icon: <IconPhone size={20} />, key: 'phone' },
+  { icon: <IconMail size={20} />, key: 'mail' }
+]
+const year = new Date().getFullYear()
 
 export function Footer () {
   return (
     <>
       <footer className="bg-primary-800 flex justify-center">
         <div className="container text-white p-4 px-4 lg:px-6 min-w-min max-w-7xl  py-14">
-          <h3 className="font-bold mb-6 text-lg">Escuela de Postgardo UNAP</h3>
+          <div className='flex  items-center gap-2'>
+            {/* <div className='w-[132.8px]'>
+              <Image src={unap.src} className='min-w-[8.3rem]' width={50} />
+            </div>
+            <Divider className='bg-white w-[2px]' orientation="vertical" /> */}
+            {/* <div className='w-[160px]'>
+              <Image src={epg.src} className='min-w-[10rem]' width={50} />
+            </div> */}
+            <div className='w-[41.59px]'>
+                <Image src={epg.src} className='min-w-[2.6rem]' width={25} />
+              </div>
+            <div className='font-bold text-[24px] leading-tight'>
+              <h3 >Escuela de Postagrado UNAP</h3>
+              {/* <h3 >Postgrado UNAP</h3> */}
+            </div>
+          </div>
+          <Divider className="my-4  bg-[#244a79]" />
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             <div className=" ">
-              <h4 className="mb-4">UBÍCANOS</h4>
+              <h4 className="mb-2 font-bold text-lg text-white">UBÍCANOS</h4>
               <ul>
                 {DataFooter.infoEpg.map((item, index) => (
-                  <li key={index} className="py-2">
+                  <li key={index} className="py-2 hover:text-yellow-400 text-[#d5dce8] text-sm flex gap-2">
+                    {icons[index].icon}
                     <Link href={item.link}>{item.label}</Link>
                   </li>
                 ))}
